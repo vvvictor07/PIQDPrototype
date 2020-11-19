72ccb59d37537306e63012da101b99269fa4d0f0
Nikita Medvedev <nmedvedev@tourmalinecore.com>
2020-11-19 17:48:21 +0500

    #cwb3xc: Inventory base added

---
+++ Assets/Scripts/Scene 2 - Game Scene/Items/Potions/HealthPotion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Scene_2___Game_Scene.Items.Templates
+{
+    [CreateAssetMenu(fileName = "HealthPotion", menuName = "Items/Potions/Health Potion", order = 51)]
+    public class HealthPotion : Item
+    {
+        public int healAmount = 25;
+
+        public override string GetDescription()
+        {
+            return description;
+        }
+
+        public override void Use()
+        {
+            Player.instance.Heal(healAmount);
+        }
+    }
+}
--- Assets/Scripts/Scene 2 - Game Scene/Player/Inventory.cs
+++ Assets/Scripts/Scene 2 - Game Scene/Player/Inventory.cs
@@ -3,51 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+public enum InventorySortingType{
+    None = 0,
+}
+
 public class Inventory : MonoBehaviour
 {
-    public List<Item> inventory = new List<Item>();
+    public List<Item> items = new List<Item>();
 
-    public bool showInventory = false;
+    public bool showInventory = true;
     private Vector2 scr;
-    private string sortType = "";
+    private InventorySortingType sortType = InventorySortingType.None;
 
-    private void Display()
+    public delegate void UpdateInventory();
+    public event UpdateInventory OnInventoryUpdate;
+
+    public static Inventory instance;
+
+    private void Awake()
     {
-        if(sortType == "")
-        {
-            if(inventory.Count <= 34)
-            {
-                for(int i = 0; i < inventory.Count; i++)
-                {
-                    if (GUI.Button(new Rect(0.5f * scr.x, 0.25f * scr.y + i * (0.25f * scr.y), 3 * scr.x, 0.25f * scr.y), inventory[i].Name))
-                    {
-                        Debug.Log(inventory[i].Name);
-                    }
-                }
-            }
-
-        }
-    }
-    private void OnGUI()
-    {
-        if (showInventory)
-        {
-            scr.x = Screen.width / 16;
-            scr.y = Screen.height / 9;
-
-            GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "");
-            string[] itemTypes = Enum.GetNames(typeof(ItemType));
-            int CountofItemTypes = itemTypes.Length;
-
-            for(int i = 0; 1 < CountofItemTypes; i++)
-            {
-                if (GUI.Button(new Rect(4 * scr.x + i * scr.x, 0, scr.x, 0.25f * scr.y), itemTypes[i]))
-                {
-                    sortType = itemTypes[i];
-                }
-            }
-            Display();
-        }
+        instance = this;
     }
 
+    private void Start()
+    {
+        Debug.Log(items.Count);
+    }
 }
